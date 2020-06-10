451abb1c16b9a2f6a8402b40d8cb09ec36735965
Adrien Escobin <adrien.escobin@gmail.com>
2020-06-10 21:12:44 +0800

    Restore some files
    accidental deletion of files

---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h1>Main Page</h1>
+        <div class="container">
+            <ul>
+                <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+// import Vue from 'vue'
+import ItemQuery from '~/apollo/queries/items.graphql'
+import "vue-apollo"
+import { Component, Vue } from 'nuxt-property-decorator'
+import { ItemsQuery } from '../types/types'
+
+@Component({
+    apollo: {
+        items: {
+            query: ItemQuery
+        }
+    }
+})
+
+export default class IndexPage extends Vue { }
+
+
+// interface Item {
+//     id: number
+//     name: string
+// }
+
+// interface Data {
+//     allItems: Item []
+// }
+
+// export default Vue.extend({
+//     data(): Data {
+//         return {
+//             allItems: []
+//         }
+//     },
+//     computed: {
+//       items(): Item[] {
+//           return this.res
+//       }
+//     },
+//     apollo: {
+//         res: () => ItemQuery
+//     }
+// })
+    
+
+</script>
---
+++ types/graphql.d.ts
@@ -0,0 +1,9 @@
+
+declare module '*/items.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const Items: DocumentNode;
+
+  export default defaultDocument;
+}
+    
